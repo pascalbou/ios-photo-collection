e11730ae89c41dec6283fb1589a977f18b581396
krikaz <pascal.bou@protonmail.com>
2020-03-10 20:47:48 +0100

    part 4.2

--- PhotoCollection/PhotoCollection/PhotosCollectionViewController.swift
+++ PhotoCollection/PhotoCollection/PhotosCollectionViewController.swift
@@ -8,82 +8,62 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
-
 class PhotosCollectionViewController: UICollectionViewController {
+    
+    let photoController = PhotoController()
+    let themeHelper = ThemeHelper()
+    
+    func setTheme() {
+        guard let themePreference = themeHelper.themePreference else { return }
+        switch themePreference {
+        case "Blue":
+            self.view.backgroundColor = .blue
+        default:
+            self.view.backgroundColor = .black
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     // MARK: UICollectionViewDataSource
 
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+        return photoController.photos.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as? PhotosCollectionViewCell else { return UICollectionViewCell() }
     
-        // Configure the cell
+        let photo = photoController.photos[indexPath.item]
+        cell.photo = photo
     
         return cell
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "AddPhotoSegue":
+            if let destinationVC = segue.destination as? PhotoDetailViewController {
+                destinationVC.themeHelper = themeHelper
+                destinationVC.photoController = photoController
+            }
+        case "DetailPhotoSegue":
+            if let destinationVC = segue.destination as? PhotoDetailViewController, let cell = sender as? PhotosCollectionViewCell {
+                destinationVC.themeHelper = themeHelper
+                destinationVC.photoController = photoController
+                destinationVC.photo = cell.photo
+            }
+        case "ThemeSegue":
+            if let destinationVC = segue.destination as? ThemeSelectionViewController {
+                destinationVC.themeHelper = themeHelper
+            }
+        default:
+            return
+        }
     }
-    */
 
 }
