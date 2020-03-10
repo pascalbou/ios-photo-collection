e9ac2ad25d1b0b672d9ea7a85a37859fe5db54b3
krikaz <pascal.bou@protonmail.com>
2020-03-10 19:37:11 +0100

    part 3.1

---
+++ PhotoCollection/PhotoCollection/PhotosCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosCollectionViewCell.swift
+//  PhotoCollection
+//
+//  Created by krikaz on 3/10/20.
+//  Copyright © 2020 thewire. All rights reserved.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoLabel: UILabel!
+    
+    
+}
